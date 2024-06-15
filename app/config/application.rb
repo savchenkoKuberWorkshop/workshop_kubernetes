91b3ad30d1bf7eb3c3d6137fe425ca2fade7f650
Sergei <sa.savchenko@sov-teh.com>
2024-06-15 19:41:47 +0500

    hw-2

--- app/config/application.rb
+++ app/config/application.rb
@@ -27,6 +27,7 @@ module App
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.active_record.sqlite3_production_warning=false
 
     # Configuration for the application, engines, and railties goes here.
     #
